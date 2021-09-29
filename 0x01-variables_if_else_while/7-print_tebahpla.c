e61b4364061bb6d4be8d40efd3065cfc7f064591
MuchoCode <3634@holbertonschool.com>
2021-09-28 18:23:39 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - this is what the main function does
+ *
+ * Return: this is what's supposed to be returned to main
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch <= 'a'; ch++)
+	putchar (ch);
+	putchar('\n');
+	return (0);
+}
