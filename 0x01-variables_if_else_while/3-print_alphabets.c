fd34f9241cfb9b7676b3ae5ff6c4ba26cd1f0571
MuchoCode <3634@holbertonschool.com>
2021-09-28 17:28:51 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - this is what the main function does
+ *
+ * Return: this is what's supposed to be returned to main
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	putchar (ch);
+	putchar('\n');
+	return (0);
+}
