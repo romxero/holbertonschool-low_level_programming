7fcb74f1f6028761e43bf9143c4dea982828843c
MuchoCode <3634@holbertonschool.com>
2021-09-28 17:17:38 -0700

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
-
+/**
+ * main - this is what the main function does
+ *
+ * Return: this is what's supposed to be returned to main
+ */
 int main(void)
 {
-char = ch
+	char ch;
+
 	for (ch = "a"; ch <= "z"; ch++)
 {
-	putchar (ch)
+	putchar (ch);
 }
-	putchar('\n')
+	putchar('\n');
 	return (0);
 }
