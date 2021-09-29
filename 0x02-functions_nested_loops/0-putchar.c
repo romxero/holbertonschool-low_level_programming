d830000916132feda944e78715d22f9882c78b33
MuchoCode <3634@holbertonschool.com>
2021-09-29 16:16:11 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,12 @@
 #include "main.h"
+/**
+ * main - this is what the main function does
+ *
+ * Return: this is what's supposed to be returned to main
+ */
 
 int main(void)
 {
-
 _putchar('_');
 _putchar('p');
 _putchar('u');
