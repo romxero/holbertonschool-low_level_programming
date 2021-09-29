0a8fb95b49564a1e86dde05396a86890c3a7e1b4
MuchoCode <3634@holbertonschool.com>
2021-09-28 18:20:48 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - this is what the main function does
+ *
+ * Return: this is what's supposed to be returned to main
+ */
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	putchar (x);
+	putchar('\n');
+	return (0);
+}
