575434933434462023f98fc9f9c6d33f615b8957
MuchoCode <3634@holbertonschool.com>
2021-10-01 08:37:36 -0700

    3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,21 @@
-#include "main.h"
+#include <stdio.h>
 /**
  * _islower - this is what the main function does
- *
+ * @c: variable
  * Return: this is what's supposed to be returned to main
  */
 
+int _islower(int c);
 
-int _islower(int c)
+
 {
-	if (c == 'c')
-	return (1);
-	else
-
-	return (0);
+  if (c >= 'a' && c <= 'z')
+    {
+      return (1);
+    }
+  else
+    {
+      return (0);
+    }
 
 }
