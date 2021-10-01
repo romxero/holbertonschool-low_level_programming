7e0758b44f032cd592483de501d08ed20cfee4fd
MuchoCode <3634@holbertonschool.com>
2021-10-01 11:41:48 -0700

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * print_sign - this is what the main function does
+ * @n: variable
+ * Return: this is what's supposed to be returned to main
+ */
+
+
+int print_sign(int n)
+
+
+{
+	if (n > 0)
+	{
+	return (1);
+	printf("+");
+
+	else if (n == 0)
+	{
+	return (0);
+	printf("0");
+	};
+
+	else
+	{
+	return (-1);
+	printf("-");
+	}
+
+}
