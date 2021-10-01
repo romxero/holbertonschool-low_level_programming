a4ee16a06caef84183e252515d45a114f2283e20
MuchoCode <3634@holbertonschool.com>
2021-10-01 15:02:32 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _abs - this is what the main function does
+ * @int: variable
+ * Return: this is what's supposed to be returned to main
+ */
+
+int _abs(int)
+
+int x;
+
+{
+	if (x < 0)
+	{
+	x * -1
+	_putchar(x);
+	return (x);
+	}
+	else
+	{
+	_putchar('x');
+	return (x);
+	}
+
+}
