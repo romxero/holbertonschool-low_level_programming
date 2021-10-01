834825e5534b9d1e220e25154546b3b43499ef97
MuchoCode <3634@holbertonschool.com>
2021-10-01 09:01:02 -0700

    m

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * int _isalpha(int c); - this is what the main function does
+ * @c: variable
+ * Return: this is what's supposed to be returned to main
+ */
+
+int _islower(int c)
+
+
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+
+}
