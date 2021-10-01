12801d2073636d65588a559f6e68d2767bdd2992
MuchoCode <3634@holbertonschool.com>
2021-10-01 16:33:22 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - this is what the main function does
+ * @x: variable
+ * @y: variable
+ * Return: this is what's supposed to be returned to main
+ */
+
+int add(int x, int y)
+{
+
+	x = x + y;
+	putchar(x);
+
+
+}
