818111bb30c3f515df31118ebf89ecd554ee6e57
MuchoCode <3634@holbertonschool.com>
2021-10-01 16:20:52 -0700

    9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * times_table - this is what the main function does
+ *
+ * Return: this is what's supposed to be returned to main
+ */
+
+
+
+void times_table(void)
+
+{
+
+
+
+  putchar
+
+  
+}
