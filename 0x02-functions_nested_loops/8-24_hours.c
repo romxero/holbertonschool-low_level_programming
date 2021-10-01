e8250af9a4624841d78f332852c46eb27a988956
MuchoCode <3634@holbertonschool.com>
2021-10-01 16:13:58 -0700

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack_bauer - this is what the main function does
+ *
+ * Return: this is what's supposed to be returned to main
+ */
+
+void jack_bauer(void)
+
+{
+
+  int hour, minute;
+
+  for (hour = 0; hour <= 23; hour++)
+
+    for (minute = 0; minute <=60; minute++)
+
+putchar
+
+  putchar
+
+  
+}
