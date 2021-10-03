f468b80c6604213076c407f73170911669a17637
MuchoCode <3634@holbertonschool.com>
2021-10-03 12:49:37 -0700

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * more_numbers - this is what the main function does
+ * @a: variable
+ * Return: this is what's supposed to be returned to main
+ */
+
+void more_numbers(void);
+
+
+{
+
+	char a;
+
+
+	for (a >= 0; a <= 14; a++)
+	{
+	  if
+		(a );
+		_putchar(a);
+		{
+		  if (a >=10 && a <= 14);
+		  _putchar (last digit)
+		    }
+
+		
+	}
+
+	_putchar(\n);
+}
