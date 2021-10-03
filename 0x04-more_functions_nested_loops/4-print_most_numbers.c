5c81560715a7318c28ad4d42c0231bc5d1fd09db
MuchoCode <3634@holbertonschool.com>
2021-10-03 12:22:36 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_most_numbers - this is what the main function does
+ * @a: variable
+ * Return: this is what's supposed to be returned to main
+ */
+
+
+void print_most_numbers(void)
+
+{
+
+	char a;
+
+  
+	for (a >= 0; a <= 9; a++)
+	{
+	  if
+		(a != 2 && a != 4);
+		_putchar(a);
+	}
+
+	_putchar(\n);
+}
