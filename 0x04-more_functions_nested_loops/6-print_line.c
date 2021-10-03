b9e9c65bcf504c6ac41cc5589c416018fd76f4fa
MuchoCode <3634@holbertonschool.com>
2021-10-03 12:55:32 -0700

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - this is what the main function does
+ * @a: variable
+ * Return: this is what's supposed to be returned to main
+ */
+
+
+void print_line(int n);
+
+{
+
+  _putchar("_");
+
+	_putchar(0);
+}
