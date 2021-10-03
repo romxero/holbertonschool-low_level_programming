6c25065556094f06788186117fa3e47068fcad15
MuchoCode <3634@holbertonschool.com>
2021-10-03 12:05:36 -0700

    3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
- * mul - this is what the main function does
+ * print_numbers - this is what the main function does
  * @a: variable
  * @b: variable
  * Return: this is what's supposed to be returned to main
  */
 
+void print_numbers(void)
 int mul(int a, int b)
 
 {
+	for (a >= 0; a <= 9; a++)
+	{
+		_putchar(a);
+	}
 
-	int c
-
-	c = a * b;
-
-	return (c);
-
+	_putchar(\n);
 }
