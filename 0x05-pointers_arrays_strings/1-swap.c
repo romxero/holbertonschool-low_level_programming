4a54b51d93710184e0b2296b93f01e614fcdadfa
MuchoCode <3634@holbertonschool.com>
2021-10-05 09:59:12 -0700

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * swap_int - this is what the main function does
+ * @a: variable
+ * @b: variable
+ * Return: this is what's supposed to be returned to main
+ */
+
+
+void swap_int(int *a, int *b)
+
+{
+
+	int c;
+	c = *a:  
+	*a = *b;
+	*b = c;	
+
+}
