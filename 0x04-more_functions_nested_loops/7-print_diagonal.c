2c95018e1e566689f6c538ff8ae5c13e443d3d87
MuchoCode <3634@holbertonschool.com>
2021-10-05 16:31:45 -0700

    7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_diagonal - this is what the main function does
+ * @n: variable
+ * Return: this is what's supposed to be returned to main
+ */
+
+void print_diagonal(int n)
+
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+	{
+	_putchar('\\');
+	}
+	_putchar('\n');
+
+}
