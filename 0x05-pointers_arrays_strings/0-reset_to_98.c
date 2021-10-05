31949fb7432de36ca0a706b020c041bf66ae5caf
MuchoCode <3634@holbertonschool.com>
2021-10-05 09:18:17 -0700

    0x05

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - this is what the main function does
+ * @n: variable
+ * Return: this is what's supposed to be returned to main
+ */
+
+
+
+void reset_to_98(int *n)
+{
+
+
+	*p = 98;
+
+}
