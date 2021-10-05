ecef6163a12d9406eac27e3b1517b9c5432a250e
MuchoCode <3634@holbertonschool.com>
2021-10-05 16:51:53 -0700

    7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,11 +9,20 @@ void print_diagonal(int n)
 
 {
 	int x;
+	int y;
 
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
 	for (x = 0; x < n; x++)
 	{
+	  for (y = 0; y < x; y++)
+		{
+		  _putchar(' ');	
+		}
 	_putchar('\\');
-	}
 	_putchar('\n');
+	}
 
 }
