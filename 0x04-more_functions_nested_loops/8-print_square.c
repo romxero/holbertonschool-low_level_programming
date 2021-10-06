988f4b8bde9b89872766229f59056cac448628ec
MuchoCode <3634@holbertonschool.com>
2021-10-06 08:19:12 -0700

    8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_square - this is what the main function does
+ * @size: variable
+ * Return: this is what's supposed to be returned to main
+ */
+
+void print_square(int size)
+
+{
+	int x;
+	int y;
+
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	for (x = 0; x < size; x++)
+	{
+	for (y = 0; y < size; y++)
+		{
+		  _putchar('#');	
+		}
+	_putchar('#');
+	_putchar('\n');
+	}
+
+}
