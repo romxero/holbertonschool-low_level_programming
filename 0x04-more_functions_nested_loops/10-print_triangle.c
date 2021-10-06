602330b5772f7c16f1e035cc91d4d623dae53c18
MuchoCode <3634@holbertonschool.com>
2021-10-06 10:25:30 -0700

    10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_triangle - this is what the main function does
+ * @size: variable
+ */
+
+
+void print_triangle(int size)
+
+{
+	int x;
+	int y;
+
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	for (x = 0; x < size; x++)
+	{
+	for (y = 0; y < size; y++)
+	{
+	_putchar('#');
+	}
+	_putchar('#');
+	_putchar('\n');
+	}
+
+}
