db02dc44bc01d360c26912e5c66975eba3ad1be4
MuchoCode <3634@holbertonschool.com>
2021-10-06 10:03:13 -0700

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * 
+ */
+
+
+int main(void)
+
+{
+
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+	  if ((a % 3 != 0) && (a % 5 != 0))
+		{
+			printf("%d ", a);
+		}
+	  	if ((a % 3 == 0) && (a % 5 != 0))
+		{
+			printf("Fizz ");
+		}
+
+	  	if ((a % 5 == 0) && (a % 3 != 0))
+		{
+			printf("Buzz ");
+		}
+
+		if ((a % 3 == 0) && (a % 5 == 0))
+		  {
+		    printf("FizzBuzz ");
+		  }
+		
+
+	}
+
+	printf("\n");
+}
