4e9d14a2a3f2108a17e50e64bd89f7bdb00172f5
MuchoCode <3634@holbertonschool.com>
2021-10-07 16:25:16 -0700

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strlen - this is what the main function does
+ * @s: variable
+ * Return: this is what's supposed to be returned to main
+ */
+
+int _strlen(char *s)
+
+
+{
+
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		*s++;
+		1++;
+	}
+
+	return (i);
+}
+
+
+}
