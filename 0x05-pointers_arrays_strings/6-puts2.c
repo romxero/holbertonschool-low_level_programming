311c106aaf868149fb9201aa3832ec24eee99be1
MuchoCode <3634@holbertonschool.com>
2021-10-07 18:13:18 -0700

    6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _puts2 - this is what the main function does
+ * puts2 - this is what the main function does
  * @str: variable
  * Return: this is what's supposed to be returned to main
  */
@@ -14,7 +14,7 @@ void puts2(char *str)
 	while (*str != '\0')
 	{
 		_putchar (*str);
-		str+=2;
+		str += 2;
 	}
 	_putchar('\n');
 
