617318279d518b2cfb9ddb0e46d0cd426b8ee8f8
MuchoCode <3634@holbertonschool.com>
2021-10-07 18:29:38 -0700

    8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * void print_array - this is what the main function does
+ * @a: variable
+ * @n: variable
+ * Return: this is what's supposed to be returned to main
+ */
+
+void print_array(int *a, int n)
+
+
+{
+
+	while (*str != '\0')
+	{
+		_putchar (*str);
+		str += 2;
+	}
+	printf ('n');
+
+}
