46309fd3aee5469caaaa3409f0d3735ef5c232df
MuchoCode <3634@holbertonschool.com>
2021-10-07 18:36:32 -0700

    9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_strcpy - this is what the main function does
+ * @dest: variable
+ * @scr: variable
+ * Return: this is what's supposed to be returned to main
+ */
+
+char *_strcpy(char *dest, char *src)
+
+
+{
+
+	int c;
+	c = *a:  
+	*a = *b;
+	*b = c;	
+
+}
