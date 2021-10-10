5672a4b1b1eab8af821414a03300158f18090163
MuchoCode <3634@holbertonschool.com>
2021-10-10 16:46:20 -0700

    2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,20 +9,27 @@
  */
 
 
+
+
 char *_strncpy(char *dest, char *src, int n)
+
+
 
 {
 	int a = 0;
 	int b = 0;
 
-	while (dest[a] != '\0')
-	a++;
-
-	while (src[b] != '\0')
+	while (src[b] != '\0' && b < n)
 	{
 		dest[a] = src[b];
 		a++;
 		b++;
+	}
+
+	while (a < n)
+	{
+	dest[a] = '\0';
+	a++;
 	}
 	return (dest);
 
