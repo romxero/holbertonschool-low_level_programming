89e2d8838451e28a6dd787b1229e6b4d8be08f89
MuchoCode <3634@holbertonschool.com>
2021-10-10 17:06:55 -0700

    3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include "main.h"
+/**
+ * _strcmp - this is what the main function does
+ * @s1: variable
+ * @s2: variable
+ * Return: this is what's supposed to be returned to main
+ */
+
+
+int _strcmp(char *s1, char *s2)
+
+
+
+
+{
+	while ((s1[a] == s2[a]) && (s1[a] != '\0') && (s2 != '\0'))
+	{
+		a++;
+	}
+
+	return(*s1 - *s2);
+
+
+}
