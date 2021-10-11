ed5d8071b214e9a1a31d9e869f10e9a32d716bf4
MuchoCode <3634@holbertonschool.com>
2021-10-11 15:22:31 -0700

    2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include "main.h"
+/**
+ * *_strchr - this is what the main function does
+ * @s: variable
+ * @c: variable
+ * Return: this is what's supposed to be returned to main
+ */
+
+
+
+char *_strchr(char *s, char c)
+
+{
+
+	int i;
+
+	for (i = 0; i++)
+	{
+		if (i = c)
+			return (c);
+		if (i = \0)
+			return ('null');
+
+	}
+}
