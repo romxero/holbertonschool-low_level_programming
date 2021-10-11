8d190612cc4ec9a9a33196434e960f926a078982
MuchoCode <3634@holbertonschool.com>
2021-10-11 15:32:01 -0700

    3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include "main.h"
+/**
+ * _strsp - this is what the main function does
+ * @s: variable
+ * @accept: variable
+ * Return: this is what's supposed to be returned to main
+ */
+
+
+
+unsigned int _strspn(char *s, char *accept)
+
+{
+
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+	s[i] = b;
+	}
+
+
+	return (s);
+
+
+}
