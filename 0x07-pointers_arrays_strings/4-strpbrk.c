3aec70233255659762f726489fe20ecd7633c8df
MuchoCode <3634@holbertonschool.com>
2021-10-11 17:46:30 -0700

    4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,17 +9,22 @@
 
 char *_strpbrk(char *s, char *accept)
 
+
 {
 
 	int i;
+	int j;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-	s[i] = b;
+		for (j = 0; accept[j] != '\0'; j++)
+		if (s[i] == accept[j])
+		{
+			return (s + i);
+		}
 	}
 
-
-	return (s);
+	return ('\0');
 
 
 }
