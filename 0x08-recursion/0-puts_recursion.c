6ccafb2a02e0523b539fbe2b98275f63708b2756
MuchoCode <3634@holbertonschool.com>
2021-10-12 12:45:28 -0700

    0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,19 +11,13 @@ void _puts_recursion(char *s)
 {
 
 
-  if (*s == '\0')
-    {
-      return;
-    }
-  else
-    {
-      _putchar("%c", *s);
-  _puts_recursion(s + 1);
-    }
-
-
-
-  _putchar('\n');
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 
 
 
