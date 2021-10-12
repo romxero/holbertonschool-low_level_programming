83053a9fb6ea6448b87b6fb8a05800633039ed09
MuchoCode <3634@holbertonschool.com>
2021-10-12 15:49:02 -0700

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,16 +12,21 @@ int _strcmp(char *s1, char *s2)
 
 {
 
-	int a;
+	int a = 0;
 
-	a = 0;
+
 
 	while ((s1[a] == s2[a]) && (s1[a] != '\0' && s2[a] != '\0'))
 	{
 		a++;
 	}
-
+	if (s1[a] == s2[a])
+	{
+	return (0);
+	}
+	else
+	{
 	return (*s1 - *s2);
-
+	}
 
 }
