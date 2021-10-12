182a2ced0f786f7384b1a033af03fc81025493d2
MuchoCode <3634@holbertonschool.com>
2021-10-11 17:57:53 -0700

    3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -17,10 +17,10 @@ unsigned int _strspn(char *s, char *accept)
 	int j;
 	int l = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0' && s[i] != ' ' && s[i] != ',' && s[i] != '.'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
-		if (s[i] == s[j])
+		if (s[i] == accept[j])
 		{
 			l++;
 		}
