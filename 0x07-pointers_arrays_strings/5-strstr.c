33716d35c253cec79445861e68ac9314d138de63
MuchoCode <3634@holbertonschool.com>
2021-10-11 18:18:56 -0700

    5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,19 +9,20 @@
 
 char *_strstr(char *haystack, char *needle)
 
-
-
 {
 
 	int i;
+	int j = 0;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-	s[i] = b;
+
+		if (haystack[i] == needle[j])
+		{
+		return (haystack + i);
+		}
 	}
 
-
-	return (s);
-
+	return ('\0');
 
 }
