c72ef3977764230d5859c107bbb21595cfb1c702
MuchoCode <3634@holbertonschool.com>
2021-10-13 11:54:44 -0700

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,25 +1,29 @@
 #include "holberton.h"
 #include "main.h"
 /**
- * *cap_string - this is what the main function does
- * @ch: variable
- * Return: this is what's supposed to be returned to main
+ * cap_string - capitalize words of a string.
+ * @s: pointer to input string.
+ *
+ * Return: Returns pointer to capitalized string.
  */
 
-char *cap_string(char ch)
-
+char *cap_string(char *s)
 {
+	int i, j;
+	char sep[] = " \t\n,;.!?\"(){}"; /* word seperators */
 
-	int a;
-
-	a = 0;
-
-	while ((s1[a] == s2[a]) && (s1[a] != '\0' && s2[a] != '\0'))
+	i = 1;
+	if (s[0] >= 'a' && s[0] <= 'z')
+	s[0] -= ('a' - 'A'); /* changes first letter of word */
+	while (s[i] != '\0')
 	{
-		a++;
+	for (j = 0; sep[j] != '\0'; j++)
+	if (s[i - 1] == sep[j] && (s[i] >= 'a' && s[i] <= 'z')) /**
+								 * checks if element before is word seperator
+								 */
+	s[i] -= ('a' - 'A'); /* capiatilzes if after seperator */
+	i++;
 	}
-
-	return (*s1 - *s2);
-
-
+	return (s);
 }
+/* code by Tim Simms */
