b68ba889af71e0d07def5f7142af187e8beb5ea2
MuchoCode <3634@holbertonschool.com>
2021-10-15 09:05:44 -0700

    6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _prime_number - this is what the main function does
+ * @n: variable
+ * Return: this is what's supposed to be returned to main
+ */
+
+int is_prime_number(int n)
+
+
+{
+
+
+
+
+
+
+}
