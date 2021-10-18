1e5201c40b03eba712d0421b18a9bfe1a9767fdd
MuchoCode <3634@holbertonschool.com>
2021-10-17 18:11:56 -0700

    9

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * *_strcpy - this is what the main function does
+ * @dest: variable
+ * @scr: variable
+ * Return: this is what's supposed to be returned to main
+ */
+
+char *_strcpy(char *dest, char *src)
+
+
+{
+
+	return(a);
+}
