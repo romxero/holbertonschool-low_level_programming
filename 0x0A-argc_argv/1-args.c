9bc20a035f5cdc2eff3d946ec6eea96e9f195ec6
MuchoCode <3634@holbertonschool.com>
2021-10-18 17:08:51 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - this is what the main function does
+ * @argc: variable
+ * @argv: variable
+ * Return: this is what's supposed to be returned to main
+ */
+
+
+int main(int argc, char *argv[] __attribute__((unused)))
+
+{
+	printf("%i\n", argc);
+	return (0);
+
+
+}
