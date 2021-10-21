ffdd62922f3e5cce0590c441ba15a9f3b96642d5
MuchoCode <3634@holbertonschool.com>
2021-10-21 12:17:02 -0700

    2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <studio.h>
+/**
+ * main - this is what the main function does
+ * Return: this is what's supposed to be returned to main
+ */
+
+
+
+
+
+void main(void)
+{
+
+	printf("%s\n", __FILE__);
+
+
+}
